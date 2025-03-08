4b36bb72b50acac03073a823e01dcf5686c3d564
luseritocraighead977 <luseritocraighead977@gmail.com>
2025-03-08 20:33:19 +0000

    Add 3.c

---
+++ src/3.c
@@ -0,0 +1,13 @@
+
+int main() {
+  int x = 5;
+  int y = 10;
+
+  if (x > y) {
+    printf("x is greater than y\n");
+  } else {
+    printf("x is less than or equal to y\n");
+  }
+
+  return 0;
+}
