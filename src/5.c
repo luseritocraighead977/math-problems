0260d521d9b91e49c67f4cdf4fe575461f2b061e
luseritocraighead977 <luseritocraighead977@gmail.com>
2025-03-10 14:57:34 +0000

    Add 5.c

---
+++ src/5.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main() {
+    int n = rand() % 10 + 1; // generate a random number between 1 and 10
+    printf("Random number: %d\n", n);
+    return 0;
+}
