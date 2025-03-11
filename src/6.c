1a742e69a324d1d6992f14e8ef0be3da7a9609fb
luseritocraighead977 <luseritocraighead977@gmail.com>
2025-03-11 16:31:55 +0000

    Add 6.c

---
+++ src/6.c
@@ -0,0 +1,13 @@
+
+int main() {
+
+    int num = 5;
+
+    if(num > 0) {
+        printf("The number is positive\n");
+    } else if (num < 0) {
+        printf("The number is negative\n");
+    } else {
+        printf("The number is zero\n");
+    }
+}
