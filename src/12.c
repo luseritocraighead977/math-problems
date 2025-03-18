0447f3f9a34df43c3f3676d725995c2f53c8106a
luseritocraighead977 <luseritocraighead977@gmail.com>
2025-03-18 09:25:13 +0000

    Add 12.c

---
+++ src/12.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void) {
+    int x;
+    printf("Enter an integer: ");
+    scanf("%d", &x);
+    if (x % 2 == 0) {
+        printf("Even\n");
+    } else {
+        printf("Odd\n");
+    }
+    return 0;
+}
